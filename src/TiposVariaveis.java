67865509f6e73c914fb97f5df0eb60071cbcefff
Perkles <rafael.arantes7@hotmail.com>
2022-10-31 19:27:57 -0300

    Tipos variaveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a classe string que representam texto de aplicação
+
+        String meuNome = "Rafael Arantes";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
     }
 }
