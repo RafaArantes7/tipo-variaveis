39277d4d907e6252fc34f3f323a0fb93c8a853c6
Perkles <rafael.arantes7@hotmail.com>
2022-11-02 17:09:02 -0300

    Operadores3.java

---
+++ src/Operadores3.java
@@ -0,0 +1,16 @@
+public class Operadores3 {
+
+    // Essa é uma forma de realizar a operacao utilizando operadores "?" e ":"
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+    }
+
+}
