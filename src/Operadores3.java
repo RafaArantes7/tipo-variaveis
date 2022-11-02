04262dc4b53ae0b0a7d1494f046ed1a3f3f4380d
Perkles <rafael.arantes7@hotmail.com>
2022-11-02 17:39:53 -0300

    Operadores3

--- src/Operadores3.java
+++ src/Operadores3.java
@@ -2,14 +2,26 @@ public class Operadores3 {
 
     // Essa é uma forma de realizar a operacao utilizando operadores "?" e ":"
     public static void main(String[] args) {
+
         int a, b;
 
-        a = 6;
+        a = 5;
         b = 6;
 
-        String resultado = a == b ? "verdadeiro" : "falso";
+        String resultado = "";
+        if (a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
 
         System.out.println(resultado);
+
+        // uma outra forma de realizar esta mesma operação de uma forma mais simples
+        // utlizando os mesmos operadores
+
+        String resultado2 = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado2);
 
     }
 
